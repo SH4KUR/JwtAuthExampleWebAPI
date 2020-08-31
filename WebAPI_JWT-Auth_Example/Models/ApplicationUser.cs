96030060106e28ae2569e3def8d7618de5ad8f05
Dmitriy Lopatiev <dmitriy.lopatiev@gmail.com>
2020-08-31 20:21:06 +0300

    add base code for project

---
+++ WebAPI_JWT-Auth_Example/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebAPI_JWT_Auth_Example.Models
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}
